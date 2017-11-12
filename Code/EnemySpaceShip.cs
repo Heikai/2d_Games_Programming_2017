330971c19a894da443102ed54e89111d459675f8
Heikai <kai-kristian.heikkila@cs.tamk.fi>
2017-11-12 23:10:22 +0200

    Created Health Power-up

---
+++ Code/EnemySpaceShip.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+	public class EnemySpaceShip : SpaceShipBase
+	{
+        [SerializeField]
+        private GameObject _powerUp;
+
+        [SerializeField]
+        private int _powerupChancePercent;
+
+        [SerializeField]
+		private float _reachedDistance = 0.5f;
+
+		[SerializeField]
+		private int _score;
+
+		private GameObject[] _movementTargets;
+		private int _currentMovementTargetIndex = 0;
+
+		public Transform CurrentMovementTarget
+		{
+			get
+			{
+				return _movementTargets[_currentMovementTargetIndex].transform;
+			}
+		}
+
+		public override Type UnitType
+		{
+			get { return Type.Enemy; }
+		}
+
+		protected override void Update()
+		{
+			base.Update();
+
+			Shoot();
+		}
+
+		protected override void Die()
+		{
+			base.Die();
+			if(LevelController.Current != null)
+			{
+				LevelController.Current.EnemyDestroyed();
+			}
+
+			GameManager.Instance.IncrementScore(_score);
+
+            int change = Random.Range(1, 100);
+            if (change <= _powerupChancePercent)
+            {
+                Instantiate(_powerUp, transform.position, transform.rotation);
+            }
+		}
+
+		public void SetMovementTargets(GameObject[] movementTargets)
+		{
+			_movementTargets = movementTargets;
+			_currentMovementTargetIndex = 0;
+		}
+
+		protected override void Move()
+		{
+			if(_movementTargets == null || _movementTargets.Length == 0)
+			{
+				return;
+			}
+
+			UpdateMovementTarget();
+			Vector3 direction =
+				(CurrentMovementTarget.position - transform.position).normalized;
+			transform.Translate(direction * Speed * Time.deltaTime);
+		}
+
+		private void UpdateMovementTarget()
+		{
+			// Have we reached our current movement target or not?
+			if( Vector3.Distance( transform.position,
+				CurrentMovementTarget.position ) < _reachedDistance )
+			{
+				// We have reached the target, let's update it.
+				if(_currentMovementTargetIndex >= _movementTargets.Length -1)
+				{
+					// we have reached the end of our path. Let's use the first target point
+					// as our next target.
+					_currentMovementTargetIndex = 0;
+				}
+				else
+				{
+					_currentMovementTargetIndex++;
+				}
+			}
+		}
+	}
+}
---
+++ Code/Health.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+	public class Health : MonoBehaviour, IHealth
+	{
+		[SerializeField] private int _initialHealth;
+		[SerializeField] private int _minHealth;
+		[SerializeField] private int _maxHealth;
+
+
+        private int _currentHealth;
+		private bool _isImmortal = false;
+
+		public int CurrentHealth
+		{
+			get
+			{
+				return _currentHealth;
+			}
+			private set
+			{
+				_currentHealth = Mathf.Clamp(value, _minHealth, _maxHealth);
+			}
+		}
+
+		public bool IsDead
+		{
+			get { return CurrentHealth == _minHealth; }
+		}
+
+		protected void Awake()
+		{
+			CurrentHealth = _initialHealth;
+		}
+
+		public void DecreaseHealth(int amount)
+		{			
+			if (!_isImmortal)
+			{
+				// CurrentHealth = CurrentHealth - amount;
+				CurrentHealth -= amount;
+            }
+		}
+
+		public void IncreaseHealth(int amount)
+		{
+            if (CurrentHealth + amount <= _maxHealth)
+            {
+                CurrentHealth += amount;
+            }
+            else
+            {
+                CurrentHealth = _maxHealth;
+            }
+			
+		}
+
+		public void SetImmortal(bool isImmortal)
+		{
+			_isImmortal = isImmortal;
+		}
+	}
+}
